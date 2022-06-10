aa2d61fd817eb0aa5e335b8c9861812b58951dbe
Victor Stepanov <victor.a.stepanov@gmail.com>
2022-06-10 07:51:18 -0700

    Add doc string and comments for the clean scene func

--- src/bpybb/utils.py
+++ src/bpybb/utils.py
@@ -25,11 +25,19 @@ def editmode():
 
 
 def purge_orphans():
+    """
+    Remove all orphan data blocks
+
+    see this from more info:
+    https://youtu.be/3rNqVPtbhzc?t=149
+    """
     if bpy.app.version >= (3, 0, 0):
+        # run this only for Blender versions 3.0 and higher
         bpy.ops.outliner.orphans_purge(
             do_local_ids=True, do_linked_ids=True, do_recursive=True
         )
     else:
+        # run this only for Blender versions lower than 3.0
         # call purge_orphans() recursively until there are no more orphan data blocks to purge
         result = bpy.ops.outliner.orphans_purge()
         if result.pop() != "CANCELLED":
@@ -40,23 +48,34 @@ def clean_scene():
     """
     Removing all of the objects, collection, materials, particles,
     textures, images, curves, meshes, actions, nodes, and worlds from the scene
+
+    Checkout this video explanation with example
+
+    "How to clean the scene with Python in Blender (with examples)"
+    https://youtu.be/3rNqVPtbhzc
     """
+    # make sure the active object is not in Edit Mode
     if bpy.context.active_object and bpy.context.active_object.mode == "EDIT":
         bpy.ops.object.editmode_toggle()
 
+    # make sure non of the objects are hidden from the viewport, selection, or disabled
     for obj in bpy.data.objects:
         obj.hide_set(False)
         obj.hide_select = False
         obj.hide_viewport = False
+
+    # select all the object and delete them (just like pressing A + X + D in the viewport)
     bpy.ops.object.select_all(action="SELECT")
     bpy.ops.object.delete()
 
+    # find all the collections and remove them
     collection_names = [col.name for col in bpy.data.collections]
     for name in collection_names:
         bpy.data.collections.remove(bpy.data.collections[name])
 
     # in the case when you modify the world shader
-    world_names = [col.name for col in bpy.data.worlds]
+    # delete and recreate the world object
+    world_names = [world.name for world in bpy.data.worlds]
     for name in world_names:
         bpy.data.worlds.remove(bpy.data.worlds[name])
     # create a new world data block
