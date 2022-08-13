716b30e5aae1cc4d00075f88517101f54d5e270c
Victor Stepanov <victor.a.stepanov@gmail.com>
2022-08-13 12:31:41 -0700

    Updating collection.py: * adding type annotations * adding an new function add_to_collection()

--- src/bpybb/collection.py
+++ src/bpybb/collection.py
@@ -1,7 +1,14 @@
+"""
+This module contains utilities working with Blender collections.
+"""
+
+from typing import Optional
+
 import bpy
+import bpy_types
 
 
-def create_collection(col_name):
+def create_collection(col_name: str) -> bpy_types.Collection:
     bpy.ops.object.select_all(action="DESELECT")
 
     bpy.ops.collection.create(name=col_name)
@@ -10,3 +17,18 @@ def create_collection(col_name):
     bpy.context.scene.collection.children.link(collection)
 
     return collection
+
+
+def add_to_collection(col_name: str, obj: Optional[bpy_types.Object] = None, base_collection: Optional[bpy_types.Collection] = None) -> None:
+    """
+    Adds a given object to a collection with col_name
+    """
+    if obj is None:
+        obj = bpy.context.active_object
+
+    if base_collection is None:
+        base_collection = bpy.context.scene.collection
+
+    bpy.ops.object.collection_link(collection=col_name)
+
+    base_collection.objects.unlink(obj)
