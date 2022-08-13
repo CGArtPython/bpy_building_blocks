1e680834e7ed0d5dfb6edf694f251831c0a94895
Victor Stepanov <victor.a.stepanov@gmail.com>
2022-08-13 12:34:32 -0700

    Update random.py: * adding apply_random_rotation()

--- src/bpybb/random.py
+++ src/bpybb/random.py
@@ -1,7 +1,15 @@
-import time
+"""
+This module contains utilities for working with random number generation.
+"""
+
+import math
 import random
+import time
 
 import bpy
+
+
+from bpybb.utils import active_object, apply_rotation
 
 
 def time_seed():
@@ -16,3 +24,16 @@ def time_seed():
     bpy.context.window_manager.clipboard = str(seed)
 
     return seed
+
+
+def apply_random_rotation():
+    """
+    Applies a random rotation on X, Y, Z for the currently active object
+    """
+    obj = active_object()
+
+    obj.rotation_euler.x = math.radians(random.uniform(0, 360))
+    obj.rotation_euler.y = math.radians(random.uniform(0, 360))
+    obj.rotation_euler.z = math.radians(random.uniform(0, 360))
+
+    apply_rotation()
