65b3700e25a1152fe92c7cb4014e67f655203a79
Victor Stepanov <victor.a.stepanov@gmail.com>
2022-08-13 12:29:18 -0700

    Adding empty.py with utilities for working with empties: * add_ctrl_empty() - for adding a control empty

---
+++ src/bpybb/empty.py
@@ -0,0 +1,20 @@
+"""
+This module contains utilities working with Blender collections.
+"""
+
+import bpy
+
+from bpybb.utils import active_object
+
+
+def add_ctrl_empty(name=None):
+
+    bpy.ops.object.empty_add(type="PLAIN_AXES", align="WORLD")
+    empty_ctrl = active_object()
+
+    if name:
+        empty_ctrl.name = name
+    else:
+        empty_ctrl.name = "empty.cntrl"
+
+    return empty_ctrl
