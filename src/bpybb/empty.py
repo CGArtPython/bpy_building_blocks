271d53d0396c4cb2cbb092fdf454145329821854
CGArtPython <cgartpython@gmail.com>
2023-02-12 11:46:24 -0800

    updates to src\bpybb\empty.py - update custom name of the ctrl empty - cleanup

--- src/bpybb/empty.py
+++ src/bpybb/empty.py
@@ -9,11 +9,11 @@ from bpybb.utils import active_object
 
 def add_ctrl_empty(name=None):
 
-    bpy.ops.object.empty_add(type="PLAIN_AXES", align="WORLD")
+    bpy.ops.object.empty_add(type="PLAIN_AXES")
     empty_ctrl = active_object()
 
     if name:
-        empty_ctrl.name = name
+        empty_ctrl.name = f"empty.{name}"
     else:
         empty_ctrl.name = "empty.cntrl"
 
